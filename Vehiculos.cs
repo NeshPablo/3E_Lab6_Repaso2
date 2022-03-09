5ac01866319caf6b9917f69c6a53529b0f58c6b2
Nesh <neshitopablo@gmail.com>
2022-03-09 17:51:35 -0600

    Agregar archivos de proyecto.

---
+++ Vehiculos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3E_Lab6_Repaso2
+{
+    internal class Vehiculos
+    {
+        public int Placa { get; set; }
+        public string Marca { get; set; }
+        public int Modelo { get; set; }
+        public string Color { get; set; }
+        public int PrecioKilometro { get; set; }
+    }
+}
